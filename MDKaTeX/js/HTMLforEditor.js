c59834a1d797c42e4e7a20c4f8e0dd8f6c67a3f7
Alex Thillen <alexthillen852@gmail.com>
2023-09-03 13:06:29 +0200

    fix dollar

--- MDKaTeX/js/HTMLforEditor.js
+++ MDKaTeX/js/HTMLforEditor.js
@@ -122,7 +122,7 @@ function show() {
 
 function renderMath(text) {
   text = replaceInString(text);
-  text = text.replaceAll('\\$', 'DOLLAR_REPLACE_ANKI');
+  text = text.replaceAll('\\$', '⛳');
   area.textContent = text;
   renderMathInElement(area, {
     delimiters: [
@@ -160,7 +160,7 @@ function markdown() {
   // render
   text = replaceHTMLElementsInString(element.innerHTML);
   text = md.render(text);
-  text = text.replaceAll('DOLLAR_REPLACE_ANKI', '$');
+  text = text.replaceAll('⛳', '$');
   // post processing
   for (let i = 0; i < clozes.length; i++) {
     text = text.replace("REPLACE_ME_ANKI", clozes[i].outerHTML);
